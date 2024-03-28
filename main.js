24c3c7e72565ac718f95028d7ea6f490dacf3c4d
Rudra Patel <159271064+Rudra-9124@users.noreply.github.com>
2024-03-29 00:19:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 function openSlideBar() {
-  document.getElementById('slideBar').style.width = '250px';
+  document.getElementById('slidebar').style.width = '250px';
 }
 
 function closeSlideBar() {
-  document.getElementById('slideBar').style.width = '0';
+  document.getElementById('slidebar').style.width = '0';
 }
 
 document.getElementById("signin").addEventListener("click", function() {
   window.location.href = "SignIn.html";
+});
+
+window.addEventListener('scroll', function() {
+  var navbar = document.getElementById('navbar');
+  var search = document.getElementById('search');
+  if (window.scrollY === 0) { 
+    navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.0)';
+    search.style.backgroundColor = 'rgba(255,255,255,0)';
+    search.style.border = '2px solid white'
+  } else {
+    navbar.style.backgroundColor = 'black';
+    search.style.backgroundColor = 'rgba(75, 75, 75, 0.719)';
+    search.style.border = 'none'
+  }
 });
